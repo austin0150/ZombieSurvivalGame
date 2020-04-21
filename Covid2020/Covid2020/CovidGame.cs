418996d27df3c9f38ca3351380df742bce1d7a19
Jason Hogan <jasonhogan5412@gmail.com>
2020-04-20 20:32:11 -0400

    Updated some of the assets and addid basic player movement and aiming.

---
+++ Covid2020/Covid2020/CovidGame.cs
@@ -0,0 +1,64 @@
+﻿using Covid2020.Assets;
+using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid2020
+{
+    class CovidGame
+    {
+        public bool GameOver { get; set; }
+        public Player player;
+
+        public CovidGame(Vector2 SpawnPos)
+        {
+            player = new Player(SpawnPos, 5);
+        }
+
+        public void Draw(CanvasDrawingSession drawingSession)
+        {
+            player.Draw(drawingSession);
+        }
+
+        public void Update()
+        {
+            player.UpdatePosition();
+        }
+
+        public void SetPlayerMoveUp(bool move)
+        {
+            player.moveUp = move;
+        }
+
+        public void SetPlayerMoveDown(bool move)
+        {
+            player.moveDown = move;
+        }
+
+        public void SetPlayerMoveLeft(bool move)
+        {
+            player.moveLeft = move;
+        }
+
+        public void SetPlayerMoveRight(bool move)
+        {
+            player.moveRight = move;
+        }
+
+        public void SetPlayerAimLocation(Vector2 position)
+        {
+            player.pointPosition = position;
+        }
+
+        public void SetBitmaps(List<CanvasBitmap> moveBitmaps,
+                               List<CanvasBitmap> reloadBitmaps)
+        {
+            player.aimingBitmaps = moveBitmaps;
+            player.reloadBitmaps = reloadBitmaps;
+        }
+    }
+}
--- Covid2020/Covid2020/Game.xaml.cs
+++ Covid2020/Covid2020/Game.xaml.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.Graphics.Canvas;
+﻿using Covid2020.Assets;
+using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
 using System.Threading.Tasks;
 using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Covid2020
 {
@@ -17,14 +20,90 @@ namespace Covid2020
     /// </summary>
     public sealed partial class Game : Page
     {
+        private CovidGame covidGame;
+
         public Game()
         {
             this.InitializeComponent();
+
+            covidGame = new CovidGame(canvas.Size.ToVector2()/2);
+
+            Window.Current.CoreWindow.KeyDown += Canvas_KeyDown;
+            Window.Current.CoreWindow.KeyUp += Canvas_KeyUp;
+            Window.Current.CoreWindow.PointerPressed += Canvas_PointerPressed;
+            Window.Current.CoreWindow.PointerMoved += Canvas_PointerMoved;
+        }
+
+        private void Canvas_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            if (args.VirtualKey == Windows.System.VirtualKey.W)
+            {
+                covidGame.SetPlayerMoveUp(true);
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.A)
+            {
+                covidGame.SetPlayerMoveLeft(true);
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.S)
+            {
+                covidGame.SetPlayerMoveDown(true);
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.D)
+            {
+                covidGame.SetPlayerMoveRight(true);
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.Escape)
+            {
+                //pause
+            }
+        }
+
+        private void Canvas_KeyUp(CoreWindow sender, KeyEventArgs args)
+        {
+            if (args.VirtualKey == Windows.System.VirtualKey.W)
+            {
+                covidGame.SetPlayerMoveUp(false);
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.A)
+            {
+                covidGame.SetPlayerMoveLeft(false);
+
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.S)
+            {
+                covidGame.SetPlayerMoveDown(false);
+
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.D)
+            {
+                covidGame.SetPlayerMoveRight(false);
+
+            }
+            if (args.VirtualKey == Windows.System.VirtualKey.Escape)
+            {
+                // pause
+            }
+        }
+
+        private void Canvas_PointerPressed(CoreWindow sender, PointerEventArgs args)
+        {
+            if (args.CurrentPoint.Properties.IsLeftButtonPressed)
+            {
+                // shoot
+            }
+        }
+
+        private void Canvas_PointerMoved(CoreWindow sender, PointerEventArgs args)
+        {
+            covidGame.SetPlayerAimLocation(args.CurrentPoint.Position.ToVector2());
         }
 
         private void Canvas_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
         {
-            //if not game over
+            if (this.covidGame.GameOver == false)
+            {
+                covidGame.Draw(args.DrawingSession);
+            }
         }
 
         private void Canvas_CreateResources(CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
@@ -34,12 +113,19 @@ namespace Covid2020
 
         async Task CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender)
         {
-            
+            List<CanvasBitmap> aimAssets = new List<CanvasBitmap>();
+            foreach (string aimAsset in GameAssets.PlayerAiming)
+            {
+                aimAssets.Add(await CanvasBitmap.LoadAsync(sender, aimAsset));
+            }
+
+            // need to add the bitmaps for reloading
+            covidGame.SetBitmaps(aimAssets, new List<CanvasBitmap>());
         }
 
         private void Canvas_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
         {
-            //call an update
+            covidGame.Update();
         }
     }
 }
